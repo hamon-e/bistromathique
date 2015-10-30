86075accdc4048ca32bc470793a9d386f495c14a
Nicolas Goudal <goudal_n@epitech.eu>
2015-10-30 01:36:36 +0100

    Automatic push

--- sources/inf_op/do_inf_op.c
+++ sources/inf_op/do_inf_op.c
@@ -5,7 +5,7 @@
 ** Login   <goudal_n@epitech.net>
 **
 ** Started on  Thu Oct 29 21:45:55 2015 Nicolas Goudal
-** Last update Fri Oct 30 01:08:30 2015 Nicolas Goudal
+** Last update Fri Oct 30 01:35:48 2015 Nicolas Goudal
 */
 
 #include <stdlib.h>
@@ -13,18 +13,31 @@
 #include "parser.h"
 #include "inf_op.h"
 
-static t_op_and_fun const	g_dico[6] =
+static t_op_and_fun const	g_dico[5] =
 {
   {OP_ADD_IDX, inf_add},
   {OP_SUB_IDX, inf_sub},
   {OP_MULT_IDX, inf_mult},
   {OP_DIV_IDX, inf_div},
   {OP_MOD_IDX, inf_mod},
-  {OP_ERROR_IDX, NULL}
 };
 
-t_op_nbr		*rec_op(t_tree *root,)
+t_op_nbr		*rec_op(t_data *ctrl, t_tree *root, t_op_nbr )
 {
+  int			i;
+  t_op_data		calc;
+
+  i = 0;
+  if (root->type == OP)
+  {
+    while (root->data->c != ctrl->op_base[g.dico[i].op])
+      ++i;
+
+  }
+  else
+  {
+     fill_calc(ctrl, &calc, );
+  }
 }
 
 t_nbr			do_inf_op(t_data *ctrl, t_tree *root)
